5404f3ab80005714e1ca233adf8ed62506b130c5
Arthur Bruno P. Gomes <arthurbruno489@gmail.com>
2025-11-17 16:44:17 -0300

    chore: Adiciona Maven Wrapper e estrutura inicial de testes

---
+++ src/test/java/com/aee/sistema_aee/SistemaAeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aee.sistema_aee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
